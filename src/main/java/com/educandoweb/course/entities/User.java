50afb8829078749abff54234e77bf620978ef8fc
Marcelo Moura <mgmoura@gmail.com>
2022-08-22 12:47:58 -0300

    Payment, One-to-One Association

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -19,6 +19,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class User implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
